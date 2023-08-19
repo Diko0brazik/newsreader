e8c87d7fadb36cedeece7265fac0adc1656ef6ae
Dikoobrazik <specef@gmail.com>
2023-08-19 12:41:28 +0200

    hwg2

---
+++ Parcing/readbloomberg.py
@@ -0,0 +1,17 @@
+from globalclassparcing import parcing_c
+
+class bloomberg(parcing_c):
+    def __init__():
+        name = 'bloomberg'
+        site = 'https:\\' # link to site
+    
+    def connection_to_site(): #TODO 1) write connection,  2) move connection to parent class
+        pass
+
+    def parcing():
+        # return plain txt from site
+        txt = " asdfdds asfdg a afg afa fdg" # TODO create parcing
+        return txt 
+    
+def parcing_class():
+    return bloomberg()
--- old/makeuniquewordid.py
+++ old/makeuniquewordid.py
@@ -1,5 +1,5 @@
-import psycopg2
-import dbconnect
+ import psycopg2
+import old.dbconnect as dbconnect
 
 
 def main():
